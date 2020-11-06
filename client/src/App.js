f8bea2e02e97eaee9a80dea19fa3011e5cec0ec0
Modurotolu Olokode <durolawk@gmail.com>
2020-11-06 21:09:56 +0100

    styled head section

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header /*className="App-header"*/>
+        <Head>
+          <img height="35em" src="https://uploads-ssl.webflow.com/5f127ac8860f158e650f06b5/5f12985a91b23596370f7a81_logo-72-dc-7-f-0-f%402x.png" alt="Remote Roofing Logo"></img>
+          <a href="tel:+1 (800) 401-7716">+1 (800) 401-7716</a>
+        </Head>
+        <nav>
+          <a href="">Home</a>
+          <a href="">FAQ</a>
+          <a href="">Services</a>
+        </nav>
+      </Header>
     </div>
   );
 }
+
+const Header = styled.header`
+  background-color: #74787a;
+  max-width: 70%;
+  margin: 0 auto;
+  text-decoration: none
+`
+const Head = styled.div`
+  justify-content: space-between;
+  display: flex;
+  align-items: center;
+`
 
 export default App;

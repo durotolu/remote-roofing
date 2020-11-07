744f9e374ed8cb5dfe57142661a9cd96824b405e
Modurotolu Olokode <durolawk@gmail.com>
2020-11-07 21:12:29 +0100

    styled the search section

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
+    <StyledApp className="App">
       <Header /*className="App-header"*/>
         <Head>
           <img src="https://uploads-ssl.webflow.com/5f127ac8860f158e650f06b5/5f12985a91b23596370f7a81_logo-72-dc-7-f-0-f%402x.png" alt="Remote Roofing Logo" />
@@ -15,7 +15,7 @@ function App() {
         </Head>
         <StyledNav>
           <div>
-            <a href="">Home</a>
+            <a href=""><span>Home</span></a>
             <a href="">FAQ</a>
             <a href="">Services</a>
           </div>
@@ -25,19 +25,35 @@ function App() {
             <h1>Free Roof Repair Inspection & Local Roofing Quote</h1>
             <h3>Get your roof inspected remotely and connect with our trusted roofers</h3>
           </div>
-          <label>
-            <input/>
-          </label>
+          <StyledSearchBar>
+            <img src="https://uploads-ssl.webflow.com/5f127ac8860f158e650f06b5/5f5a6d20aa3c71469c732751_blue-home-pin.png" alt="Enter your address for Free Roof Inspection" />
+            <div>
+              <label>
+                <input placeholder="Your Address" />
+              </label>
+              <button>Get Inspection</button>
+            </div>
+          </StyledSearchBar>
         </StyledHeading>
       </Header>
-    </div>
+    </StyledApp>
   );
 }
 
+const StyledApp = styled.div`
+  background-color: #74787a;
+  border: 2px solid black;
+  background-image: url("https://uploads-ssl.webflow.com/5f127ac8860f158e650f06b5/5f5923b3f775ad961072248b_rr-bg-1920w-optimized.png");
+  background-size: 1500px;
+  background-repeat: no-repeat;
+  /* height:570px; */
+  /* width: 3000px; */
+  background-position: center;
+`
+
 const Header = styled.header`
   color: white;
-  background-color: #74787a;
-  max-width: 68%;
+  max-width: 68.5%;
   margin: 0 auto;
   text-decoration: none;
 `
@@ -45,16 +61,16 @@ const Head = styled.div`
   justify-content: space-between;
   display: flex;
   align-items: center;
-  margin: 1.5em auto;
+  margin: 1.3em auto;
 
   img {
-    height: 2.5em;
+    height: 2em;
   }
 
   div {
     display: flex;
     align-items: center;
-    margin-right: 3.5em;
+    margin-right: 2%;
     img {
       height: 1em;
       margin-right: 0.5em
@@ -71,28 +87,83 @@ const Head = styled.div`
 const StyledNav = styled.nav`
   margin: 2.5em auto;
   div {
-    max-width: 30%;
+    max-width: 26%;
     display: flex;
-    justify-content: space-between;
+    /* justify-content: space-between; */
     text-decoration: none;
 
     a {
+    margin-right: 1.5em;
     text-decoration: none;
     color: white;
     font-size: 1.3em;
+
+    span {
+      border-bottom: 3px solid;
+      padding-bottom: 10px;
+    }
     }
   }
 `
 
 const StyledHeading = styled.div`
   text-align: left;
+  margin: 4.5em 0;
 
   h1 {
-    font-size: 2.6em;
+    font-size: 2.65em;
+    margin-bottom: 0.5em;
   }
 
   h3 {
-    font-size: 1.2em;
+    font-size: 1.22em;
+  }
+`
+
+const StyledSearchBar = styled.div`
+  max-width: 35rem;
+  height: 3.9em;
+  border-top-left-radius: 5% 50%;
+  border-top-right-radius: 5% 50%;
+  border-bottom-left-radius: 5% 50%;
+  border-bottom-right-radius: 5% 50%;
+  background-color: white;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin-top: 3em;
+
+  img {
+    border: 2px solid black;
+    height: 1.9em;
+    margin-left: 0.5em;
+  }
+
+  div {
+    border: 2px dotted black;
+    display: flex;
+    align-items: center;
+    width: 500px;
+
+    input {
+      height: 3em;
+      border: none;
+    }
+
+    input:focus {
+      outline: 1px solid #017afe;
+    }
+
+    button {
+      /* width: 30%; */
+      height: 3.5em;
+      background-color: #017afe;
+      color: white;
+      border-top-left-radius: 10% 50%;
+      border-top-right-radius: 10% 50%;
+      border-bottom-left-radius: 10% 50%;
+      border-bottom-right-radius: 10% 50%;
+    }
   }
 `
 

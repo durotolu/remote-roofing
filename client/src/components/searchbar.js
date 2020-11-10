b398dc20365da2796aa7bc74316c8a7f0539fdac
Modurotolu Olokode <durolawk@gmail.com>
2020-11-10 18:19:43 +0100

    built searchbar dropdown

--- client/src/components/searchbar.js
+++ client/src/components/searchbar.js
@@ -1,40 +1,41 @@
 import React, { useState } from "react";
+import axios from "axios";
 import { StyledSearchBar } from '../AppStyle';
-import usePlacesAutocomplete, { getGeocode, getLatLng } from "use-places-autocomplete";
 import home_pin from "../images/blue-home-pin.png";
 
 function Searchbar() {
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState([]);
   // const debouncedSearch = useDebounce(search, 700);
-  const { ready, value, suggestions: { loading, status, data }, setValue } = usePlacesAutocomplete();
 
   const onChangeSearch = e => {
     const input = e.target.value;
-    setValue(input);
-    console.log({
-      ready,
-      value,
-      loading,
-      status,
-      data,
-      setValue
-    })
-    // setSearch(value);
-    // axios.get("https://maps.googleapis.com/maps/api/place/autocomplete/xml?input=Paris&types=geocode&key=AIzaSyDy6b4AjIUft688RxkPR9fbZ2s9PP9BmUQ")
-    //   .then(res => {
-    //     console.log(res)
-    //   }).catch(err => {
-    //     console.log(err)
-    //   })
+
+    axios.post("http://localhost:4500/address", { search: input })
+      .then(res => {
+        const data = res.data
+        console.log(data)
+        setSearch(data)
+      }).catch(err => {
+        console.log(err)
+      })
   };
 
   return (
     <StyledSearchBar>
-      {/* <LocationAutocomplete  /> */}
       <div>
         <img src={home_pin} alt="Enter your address for Free Roof Inspection" />
         <label>
-          <input value={value} placeholder="Your Address" maxLength="256" onChange={onChangeSearch} />
+          <input list="locations" placeholder="Your Address" maxLength="256" onChange={onChangeSearch} onClick={null} />
+          <datalist name="locations" id="locations">
+            {search.map(location =>
+              <option value={location}>{location}</option>
+            )}
+            {/* <option value="Lagos">Lagos</option>
+            <option value="Ibadan">Ibadan</option>
+            <option value="london">london</option>
+            <option value="cali">cali</option>
+            <option value="swag">swag</option> */}
+          </datalist>
         </label>
       </div>
       <button>Get Inspection</button>
